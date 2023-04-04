56af5a6ed87705315573a84c3b83256ad9458608
Petja-Murnik <petja.murnik@gmail.com>
2023-04-04 19:31:29 +0200

    Ustvaril potrebne stvari, da nam vsaj naredi spletno stran

---
+++ auth_public.py
@@ -0,0 +1,4 @@
+db = 'opb2023_petjam'
+host = 'baza.fmf.uni-lj.si'
+user = 'petjam'
+password = 'df85iwzg'
---
+++ neki.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# uvozimo bottle.py
+from bottleext import get, post, run, request, template, redirect, static_file, url
+
+# uvozimo ustrezne podatke za povezavo
+import auth_public as auth
+
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+import os
+
+# privzete nastavitve
+SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
+RELOADER = os.environ.get('BOTTLE_RELOADER', True)
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+
+#za debugiranje
+#debuger(True)
+
+@get('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='static')
+
+
+@get('/')
+def index():
+    return template('zacetna.html', osebe=cur)
+
+
+
+
+######################################################################
+# Glavni program
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# poženemo strežnik na podanih vratih, npr. http://localhost:8080/
+if __name__ == "__main__":
+    run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
+
