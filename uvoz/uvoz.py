e4e202b2679ccff0c0d1955ec217e6e9bbd234e6
Petja-Murnik <petja.murnik@gmail.com>
2023-04-24 10:08:10 +0200

    popravil osnovne tabelce in njihov uvoz

--- uvoz/uvoz.py
+++ uvoz/uvoz.py
@@ -16,10 +16,13 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 def ustvari_tabelo_zaposlenih():
     cur.execute("""
         CREATE TABLE zaposleni (
-        ime_priimek TEXT NOT NULL,
+        ime TEXT NOT NULL,
+        priimek TEXT NOT NULL,
         naslov TEXT NOT NULL,
         mesto TEXT NOT NULL,
         TRR TEXT PRIMARY KEY,
+        uporabnisko_ime TEXT NOT NULL UNIQUE,
+        geslo TEXT NOT NULL,
         placa NUMERIC NOT NULL,
         stevilo_ur NUMERIC NOT NULL
         );            
@@ -39,26 +42,29 @@ def uvozi_podatke_zaposlenih():
         for r in rd : 
             cur.execute("""
             INSERT INTO zaposleni
-            (ime_priimek , naslov, mesto , TRR , placa, stevilo_ur)
+            (ime ,priimek , naslov, mesto , TRR,uporabnisko_ime , geslo , placa, stevilo_ur)
             VALUES
-            (%s, %s , %s , %s , %s,%s)
+            (%s, %s , %s , %s , %s,%s, %s, %s,%s);
             """ , r)
     conn.commit()
 
-#pobrisi_tabelo_zaposlenih()
-#ustvari_tabelo_zaposlenih()
-#uvozi_podatke_zaposlenih()
 
-#####################################
+pobrisi_tabelo_zaposlenih()
+ustvari_tabelo_zaposlenih()
+uvozi_podatke_zaposlenih()
+
+#####################################KUPCI
 
 def ustvari_tabelo_kupcev():
     cur.execute("""
         CREATE TABLE kupci(
-        ime_priimek TEXT NOT NULL, 
+        ime TEXT NOT NULL, 
+        priimek TEXT NOT NULL,
         naslov TEXT NOT NULL,
         mesto TEXT NOT NULL,
-        drzava TEXT NOT NULL,
-        TRR TEXT PRIMARY KEY 
+        TRR TEXT PRIMARY KEY,
+        uporabnisko_ime TEXT NOT NULL UNIQUE,
+        geslo TEXT NOT NULL 
         ) ;
     """)
     conn.commit()
@@ -67,6 +73,7 @@ def pobrisi_tabelo_kupcev():
     cur.execute("""
     DROP TABLE kupci ;
     """)
+    conn.commit()
 
 def uvozi_podatke_kupcev():
     with open("podatki/kupci.csv" , encoding  = "utf8" ,errors = "ignore") as m :
@@ -75,19 +82,29 @@ def uvozi_podatke_kupcev():
         for r in rd :
             cur.execute("""
             INSERT INTO kupci 
-            (ime_priimek , naslov, mesto , drzava , TRR)
+            (ime, priimek , naslov, mesto , TRR ,uporabnisko_ime , geslo)
             VALUES
-            (%s ,%s ,%s ,%s, %s)
+            (%s ,%s ,%s ,%s, %s, %s,%s);
             """, r)
     conn.commit()
 
-#pobrisi_tabelo_kupcev()
-#ustvari_tabelo_kupcev() 
-#uvozi_podatke_kupcev()
+pobrisi_tabelo_kupcev()
+ustvari_tabelo_kupcev() 
+uvozi_podatke_kupcev()
     
 ################ Tabele (povezovalne)
 ##Povezovalna tabela med [zaposleni(ključ TRR); vloga ;oddelek] 
+def pomoc_dodaj_zaposleni():
+    r = ["petja","murnik", "nkei","jsc","123","tsive0","asla",12,12]
+    cur.execute("""
+        INSERT INTO zaposleni
+        (ime ,priimek , naslov, mesto , TRR,uporabnisko_ime , geslo , placa, stevilo_ur)
+        VALUES
+        (%s, %s,%s,%s,%s,%s,%s,%s,%s);
+        """,r)
+    conn.commit()
 
+#pomoc_dodaj_zaposleni()    
 
 def pomoc():
     df = pd.read_excel("podatki/Tabele.xlsx" ,sheet_name="Sheet2",skiprows=[0])
